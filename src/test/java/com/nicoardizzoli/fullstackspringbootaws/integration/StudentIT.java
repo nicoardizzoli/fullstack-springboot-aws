27b6d7bef61805a9e86fbe611fa0b020f0ef08f2
nicoardizzoli <nicolas.ardizzoli@gmail.com>
2022-05-14 20:23:26 -0300

    removed h2 database

--- src/test/java/com/nicoardizzoli/fullstackspringbootaws/integration/StudentIT.java
+++ src/test/java/com/nicoardizzoli/fullstackspringbootaws/integration/StudentIT.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(
-        locations = "classpath:application-it.properties"
-)
+//@SpringBootTest
+//@TestPropertySource(
+//        locations = "classpath:application-it.properties"
+//)
 public class StudentIT {
 
 
